16444ea0c55a563de55bdca393209ef87e414a4f
Nick Telsan <nicktelsan@Nicks-MacBook-Pro.local>
2023-03-16 21:18:22 -0400

    [#] Intial Clean Up
    - Cleans up bad routes
    - Adjusts twind config

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,23 +1,15 @@
 import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Ashley Telsan</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
+      <div class="flex justify-center h-screen w-full bg-green-100 pt-24">
+        <h1 class="font-semibold text-xl">
+          Excuse the... nothingness! More coming soon.
+        </h1>
       </div>
     </>
   );
--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,12 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    colors: {
+      green: {
+        100: "#285943",
+      },
+    },
+    extend: {},
+  },
 } as Options;
